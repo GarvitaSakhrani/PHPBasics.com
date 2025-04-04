f51e5c5a1e6d29d15cdbf45437d300b9df8358f9
Garvita Sakhrani <garvita.sakhrani@innoraft.com>
2025-04-04 17:54:31 +0530

    Fres-101:Completed first task.

---
+++ js/index.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+  $('#fname , #lname').keyup(function(){
+  var fname = $('#fname').val();
+  var lname = $('#lname').val();
+  var fullname = fname + " " + lname;
+  $('#fullname').val(fullname);
+  });
+  });
+  
+  
