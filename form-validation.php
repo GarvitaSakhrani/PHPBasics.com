f60a7f88751f2ad83e1ba02afae6929051221dbf
Garvita Sakhrani <garvita.sakhrani@innoraft.com>
2025-04-05 21:53:44 +0530

    Fres-101:Completed Second Task.

--- form-validation.php
+++ form-validation.php
@@ -1,30 +1,31 @@
 <?php
 
 class form{
-  // variables declared as empty string
+
   public $fname , $lname = "";
-  public $fnameErr , $lnameErr = "";
+  public $fnameErr , $lnameErr , $imageErr= "";
+  public $image_path="";
   
   // function to check obtained values
   public function validate(){
 
       // First Name validation
       if (empty($_POST["fname"])) {
-        $this->fnameErr = "First Name is required";
+        $this->fnameErr = "First Name is required.";
       } else {
         $this->fname = $this->test_data($_POST["fname"]);
         if (!preg_match("/^[a-zA-Z ]*$/", $this->fname)) {
-          $this->fnameErr = "Only alphabets are allowed for First Name";
+          $this->fnameErr = "Only alphabets are allowed for First Name.";
         }
       }
     
       // Last Name validation
       if (empty($_POST["lname"])) {
-        $this->lnameErr = "Last Name is required";
+        $this->lnameErr = "Last Name is required.";
       } else {
         $this->lname = $this->test_data($_POST["lname"]);
         if (!preg_match("/^[a-zA-Z ]*$/", $this->lname)) {
-          $this->lnameErr = "Only alphabets are allowed for Last Name";
+          $this->lnameErr = "Only alphabets are allowed for Last Name.";
         }
       }
     }
@@ -35,11 +36,35 @@ class form{
       $data = htmlspecialchars($data);
       return $data;
     }
-  
+    // function to validate image
+    public function imgcheck(){
+      if(!isset($_FILES['image'])){
+        $this->imageErr = "Upload an image.";
+      }
+      else{
+        $image_dir = "./uploads";
+        $image_file = $image_dir . "/" . basename($_FILES["image"]["name"]);
+        $imageFileType = strtolower(pathinfo($image_file,PATHINFO_EXTENSION));
+        $extensions = array("jpeg","jpg","png","gif"); 
+      
+        if(!in_array($imageFileType, $extensions)) {      
+            $this->imageErr = "File type not supported.";
+            return;
+      }
+      if (move_uploaded_file($_FILES["image"]["tmp_name"], $image_file)) {
+        $this->image_path=$image_file;
+        
+    } else {
+        $this->imageErr="Sorry, there was an error uploading your file.";
+    }
+
+    }
+  }
 }
 // Creates object when the form is submitted successfully.
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
   $input = new form();
+  $input->imgcheck();
   $input->validate();
 }
 
--- index.php
+++ index.php
@@ -4,7 +4,7 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Form</title>
-  <!-- link for embedding jquery -->
+  <!-- link for embedding jquey -->
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
   <script src="./js/index.js"></script>
   <!-- css to display error message -->
@@ -17,7 +17,7 @@
 <?php require 'form-validation.php';?>
 
 <!-- Form to input First Name, Last Name and display Full Name -->
-<form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
+<form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" enctype="multipart/form-data">
   <label for="fname">First Name:</label>
   <input type="text" id="fname" name="fname">
   <span class="error">* <?php echo $input->fnameErr;?></span>
@@ -31,14 +31,24 @@
   <label for="fullname">Full Name:</label>
   <input type="text" id="fullname" name="fullname" readonly>
   <br><br>
+  
+  <label for="image">Choose Image:</label>
+  <input type="file" name="image" id="image">
+  <span class="error">* <?php echo $input->imageErr;?></span>
+  <br><br>
 
   <input type="submit" value="Submit">
 </form>
 
-<!-- If both fields are error free then set Full Name -->
-<?php if (!empty($input->fname) && !empty($input->lname) && empty($input->fnameErr) && empty($input->lnameErr)) {
+<!-- Output to be displayed -->
+<?php
+  if(!empty($input->image_path)){
+    echo"<img width=\"600\" height=\"600\" src =\"$input->image_path\" alt =\"Uploaded Image by user\" title=\"Image\">";
+  }
+  if (!empty($input->fname) && !empty($input->lname) && empty($input->fnameErr) && empty($input->lnameErr)) {
       echo "<h1>Hello, $input->fname " . "$input->lname</h1>";
-  }?>
+  }
+  ?>
 
 </body>
 </html>
