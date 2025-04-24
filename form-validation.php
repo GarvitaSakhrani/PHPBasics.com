9eb73ed821e5e301b3cd7f9a040157ecca76ca94
Garvita Sakhrani <garvita.sakhrani@innoraft.com>
2025-04-24 16:51:39 +0530

    Fres-101:Added Frontend Validation and styling.

--- form-validation.php
+++ form-validation.php
@@ -45,6 +45,9 @@ class form{
       }
       else{
         $image_dir = "./uploads";
+        if(!file_exists($image_dir)){
+          mkdir($image_dir,0777,true);
+        }
         $image_file = $image_dir . "/" . basename($_FILES["image"]["name"]);
         $imageFileType = strtolower(pathinfo($image_file,PATHINFO_EXTENSION));
         $extensions = array("jpeg","jpg","png","gif"); 
